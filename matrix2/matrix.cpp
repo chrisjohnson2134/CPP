b5a64c34d3c14a22a5f0a4a2ccaf7d26a239c90b
chrisjohnson2134 <cj106@evansville.edu>
2020-07-21 22:26:56 -0500

    Finally Fixed Parameter passing.

--- matrix2/matrix.cpp
+++ matrix2/matrix.cpp
@@ -7,136 +7,117 @@
 using namespace std;
 
 //passed arguments for threads structure
-struct arg_struct{
-  float* mat_a;
-  float* mat_b;
-  float* mat_c;
-  int N;
+struct arg_struct
+{
+  float *mat_a;
+  float *mat_b;
+  float *mat_c;
+  int Rows;
 };
 
 mutex mtx;
 
 //only edited/saved under a mutex lock
-int CURRENT_ROW=0;
+int CURRENT_ROW = 0;
 
 //The multiply function is passed the args structure by reference
 //the function saves the current row that needs to be processed by the
 //program and then increments it so that other threads won't do computations
 //on the same row.
-void *multiply(void *passed) 
+void *multiply(void *passed)
 {
   struct arg_struct *pass = (struct arg_struct *)passed;
-  int thread_row=0;
-
+  int thread_row = 0;
   //the row must be saved and edited under a mutex
   //so that the two threads aren't doing the same
   //row at the same time.
   mtx.lock();
-  thread_row=CURRENT_ROW;
+  thread_row = CURRENT_ROW;
   CURRENT_ROW++;
   mtx.unlock();
 
   //the function will not quite until all rows have been computed
-  while(thread_row < pass->N)
+  while (thread_row < pass->Rows)
+  {
+    for (int j = 0; j < pass->Rows; j++)
     {
-      for (int j = 0; j < pass->N; j++) 
-	{ 
-	  for (int k = 0; k < pass->N; k++)
-	    {
-	      pass->mat_c[thread_row*pass->N+j] += pass->mat_a[thread_row*pass->N+k] *  
-		pass->mat_b[k*pass->N+j];
-	    }
-	}
-      
-      mtx.lock();
-      thread_row=CURRENT_ROW;
-      CURRENT_ROW++;
-      mtx.unlock();
+      for (int k = 0; k < pass->Rows; k++)
+      {
+        pass->mat_c[thread_row * pass->Rows + j] += pass->mat_a[thread_row * pass->Rows + k] *
+                                                    pass->mat_b[k * pass->Rows + j];
+      }
     }
-  
 
+    mtx.lock();
+    thread_row = CURRENT_ROW;
+    CURRENT_ROW++;
+    mtx.unlock();
+  }
 }
 
 //the threadRipper function spawns the number of threads asked for by the user
 //and passing the reference of each of the matrices into the threads for
 //computation.
-void threadRipper(float* mat_a,float* mat_b,float* mat_c,int N,int cores)
+void threadRipper(arg_struct *pass, int cores)
 {
-  struct arg_struct pass;
-  pass.mat_a = mat_a;
-  pass.mat_b = mat_b;
-  pass.mat_c = mat_c;
-  pass.N = N;
-  
-  int num_cores = cores;
-  cout<<cores<<endl;
-  pthread_t threads[num_cores];
-  
-  for(int i=0;i<num_cores;i++)
-    {
-      pthread_create(&threads[i],NULL,multiply,(void *)&pass);
-    }
+  pthread_t threads[cores];
+  for (int i = 0; i < cores; i++)
+  {
+    pthread_create(&threads[i], NULL, multiply, (void *)pass);
+  }
 
-  for(int i=0;i<num_cores;i++)
-    {
-      pthread_join(threads[i],NULL);
-    }
+  for (int i = 0; i < cores; i++)
+  {
+    pthread_join(threads[i], NULL);
+  }
 }
-
 
 //parameter guide
 //1 -> m - multi core , S - single core
 //2 -> size matrix
 //3 -> number of cores
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-  int i,j;
-  int N = atoi(argv[2]);
-  int cores = atoi(argv[3]);
+  int i, j;
+  int Rows = atoi(argv[2]);
+  int cores = 0;
+  if(argc == 4)
+    cores = atoi(argv[3]);
+    
   srand(time(NULL));
-  struct arg_struct* pass = (struct arg_struct*)malloc(sizeof(struct arg_struct));
-  pass->mat_a = (float*)malloc(N*N*sizeof(float));
-  pass->mat_b = (float*)malloc(N*N*sizeof(float));
-  pass->mat_c = (float*)malloc(N*N*sizeof(float));
-  pass->N = N;
-  
-  //intialize the matrice's
-  for (i = 0; i < N; i++) 
-    { 
-      for (j = 0; j < N; j++)
-	{
-	  pass->mat_a[N*i+j] = i+1;//rand()%10;//i+1
-	  pass->mat_b[N*i+j] = i+1;//rand()%10;//i+1
-	   pass->mat_c[N*i+j] = 0;
-	}
-    }
+  struct arg_struct *pass = (struct arg_struct *)malloc(sizeof(struct arg_struct));
+  pass->mat_a = (float *)malloc(Rows * Rows * sizeof(float));
+  pass->mat_b = (float *)malloc(Rows * Rows * sizeof(float));
+  pass->mat_c = (float *)malloc(Rows * Rows * sizeof(float));
+  pass->Rows = Rows;
 
-  if(strcmp(argv[1],"m")==0)
-    threadRipper(pass->mat_a,pass->mat_b,pass->mat_c,N,cores);
-  else if(strcmp(argv[1],"s")==0)
+  //intialize the matrice's
+  for (i = 0; i < Rows; i++)
+  {
+    for (j = 0; j < Rows; j++)
     {
-      for(int i = 0; i < N; i++)
-	{
-	  for (int j = 0; j < N; j++) 
-	    { 
-	      for (int k = 0; k < N; k++)
-		{
-		  pass->mat_c[i*N+j] += pass->mat_a[i*N+k] *  
-		    pass->mat_b[k*N+j];
-		}
-	    }
-	}
+      pass->mat_a[Rows * i + j] = i + 1; //rand()%10;//i+1
+      pass->mat_b[Rows * i + j] = i + 1; //rand()%10;//i+1
+      pass->mat_c[Rows * i + j] = 0;
     }
-  
-  //to check the correctness of the matrix operation
-  //we can print out the computed matrix.
-  // cout << "Result matrix is \n"; 
-  // for (i = 0; i < N; i++) 
-  //   { 
-  //     for (j = 0; j < N; j++) 
-  // 	cout << pass->mat_c[N*i+j] << " "; 
-  //     cout << "\n"; 
-  //   }
+  }
+
+  if (strcmp(argv[1], "m") == 0)
+    threadRipper(pass, cores);
+  else if (strcmp(argv[1], "s") == 0)
+  {
+    for (int i = 0; i < Rows; i++)
+    {
+      for (int j = 0; j < Rows; j++)
+      {
+        for (int k = 0; k < Rows; k++)
+        {
+          pass->mat_c[i * Rows + j] += pass->mat_a[i * Rows + k] *
+                                    pass->mat_b[k * Rows + j];
+        }
+      }
+    }
+  }
 
   free(pass->mat_a);
   free(pass->mat_b);
